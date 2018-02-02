06e90c3f1e0d93059fd696f60848406c9135bf3a
redaner <rsabanovic1@gmail.com>
2018-02-02 19:21:01 +0100

    ABHP-0002: Ember Essentials (#2)
    * Ember essentials
    
    * Ember inject as service import, empty application.hbs
    
    * v1 removed
    
    * v1 in index.js route fix

---
+++ Ember/app/routes/index.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import {inject as service} from '@ember/service';
+
+export default Route.extend({
+	ajax: service(),
+
+	model () {
+		return RSVP.hash({
+			okMessage: this.get('ajax').request('/v1/health')
+		})
+	}
+});
